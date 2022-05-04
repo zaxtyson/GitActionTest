149003c4e2b09e297a997491f4d0274f90e1c1e1
zaxtyson <zaxtyson@foxmail.com>
2022-05-04 21:22:04 +0800

    upload to repo server

--- .github/workflows/scripts/upload_to_repo.sh
+++ .github/workflows/scripts/upload_to_repo.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
-apt-get install -y tree >/dev/null
-echo "Workdir: $(pwd)"
-tree ./artifact
+function install_dependencies() {
+    echo "Installing dependencies..."
+    apt-get update
+    apt-get install -y curl >/dev/null
+}
+
+function upload_to_repo() {
+    repo_api="http://repo.zaxtyson.cn:8086/v1/upload"
+
+    # compress the deb packages
+    echo "Compressing deb packages..."
+    tar -zcvf artifact.tar.gz artifact
+    echo ""
+
+    # Upload to repo server
+    cnt=1
+    max_retry=3
+    while true; do
+        echo "[$cnt] Uploading to $repo_api ..."
+        resp=$(curl -s -X PUT -d @artifact.tar.gz -H "Authorization: token $UPLOAD_TOKEN" $repo_api)
+        cnt=$((cnt + 1))
+
+        if [ "$resp"x == "ok"x ]; then
+            echo "Upload success!"
+            exit 0
+        fi
+
+        echo "Server response: $resp"
+        if [ $cnt -le $max_retry ]; then
+            echo "Upload failed, retry in 5 seconds..."
+            echo ""
+            sleep 1
+        else
+            echo "Upload failed!"
+            exit 1
+        fi
+    done
+}
+
+function main() {
+    install_dependencies
+    upload_to_repo
+}
+
+main
